6cefe329e633a1a981be3d7068d9fdb64fa4619c
Richard Rietdijk <richardrietdijk@gmail.com>
2020-08-13 14:15:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
+import Message from './Message';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chat App</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+            disabled={!input}
+          >
+            Send
+          </Button>
+        </FormControl>
+      </form>
+      {messages.map((message) => (
+        <Message text={message} />
+      ))}
     </div>
   );
 }
